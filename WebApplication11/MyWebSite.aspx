7489018766bdef1e72a072d697acbc02598043ec
Green88 <taniagr0808@gmail.com>
2013-11-05 14:22:24 +0200

    Test Assignment 1

---
+++ WebApplication11/MyWebSite.aspx
@@ -0,0 +1,119 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyWebSite.aspx.cs" Inherits="WebApplication11.WebForm2" %>
+
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit.HTMLEditor" tagprefix="cc1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<style type="text/css">  
+        .accordion {  
+            width: 400px;  
+        }  
+          
+        .accordionHeader {  
+            border: 1px solid #2F4F4F;  
+            color: white;  
+            background-color: #2E4d7B;  
+            font-family: Arial, Sans-Serif;  
+            font-size: 12px;  
+            font-weight: bold;  
+            padding: 5px;  
+            margin-top: 5px;  
+            cursor: pointer;  
+        }  
+          
+        .accordionHeaderSelected {  
+            border: 1px solid #2F4F4F;  
+            color: white;  
+            background-color: #5078B3;  
+            font-family: Arial, Sans-Serif;  
+            font-size: 12px;  
+            font-weight: bold;  
+            padding: 5px;  
+            margin-top: 5px;  
+            cursor: pointer;  
+        }  
+          
+        .accordionContent {  
+            background-color: #FFFFFF;  
+            border: 1px dashed #2F4F4F;  
+            border-top: none;  
+            padding: 5px;  
+            padding-top: 10px;  
+        }  
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+        </asp:ToolkitScriptManager>
+    
+
+    <asp:TabContainer ID="TabContainer1" runat="server" ActiveTabIndex="2" 
+        Height="312px" Width="856px">
+
+        <asp:TabPanel runat="server" HeaderText="Edit File" ID="EditorTab">
+            <ContentTemplate>
+            <div>
+                <asp:Button ID="btLoadFile" runat="server" Text="Load file" 
+                    style="margin-bottom: 10px" onclick="btLoadFile_Click"/>
+                <asp:TextBox ID="TextBoxEditor" runat="server" Height="151px" Width="828px" 
+                    ></asp:TextBox>
+                <asp:HtmlEditorExtender ID="HtmlEditorExt" runat="server" 
+                    Enabled="True" TargetControlID="TextBoxEditor">
+                </asp:HtmlEditorExtender>
+                <asp:Button ID="Button1" runat="server" Height="32px" style="margin-top: 10px" 
+                    Text="Ready? Now pick a date" Width="211px" onclick="Button1_Click" />
+                    
+                    </div>
+            </ContentTemplate>
+        </asp:TabPanel>
+
+        <asp:TabPanel ID="CalendarTab" runat="server" HeaderText="Pick a date">
+        <ContentTemplate>
+        <gridview>
+            <div>
+            <asp:TextBox ID="TextBoxCalendar" runat="server" Height="30px" Width="232px" 
+                style="margin-top: 0px"></asp:TextBox>
+            <asp:CalendarExtender ID="CalendarExt" runat="server" 
+                Enabled="True" TargetControlID="TextBoxCalendar" 
+                TodaysDateFormat = "d MMMM, yyyy" PopupButtonID="btShowCalendar" 
+                Format="D">
+            </asp:CalendarExtender>
+        
+            <asp:ImageButton ID="btShowCalendar" runat="server" Height="31px" 
+                ImageUrl="~/Images/Calendar-icon.png" style="margin-top: 0px" Width="32px" 
+                 />
+                <br />
+                <br />
+            <asp:Button ID="Button2" runat="server" onclick="Button2_Click" 
+                    Text="Picked a date? Now publish" Width="242px" />
+                </gridview>
+        </div>
+        </ContentTemplate>
+        </asp:TabPanel>
+
+        <asp:TabPanel ID="SummaryTab" runat="server" HeaderText="Ready to publish">
+        <ContentTemplate>
+            <asp:Accordion 
+            ID="Accordion1" 
+            CssClass="accordion"  
+            HeaderCssClass="accordionHeader"  
+            HeaderSelectedCssClass="accordionHeaderSelected"  
+            ContentCssClass="accordionContent" runat="server" Width="780px" 
+                FramesPerSecond="30" TransitionDuration="250">
+            </asp:Accordion>    
+        
+        </ContentTemplate>
+        </asp:TabPanel>
+    </asp:TabContainer>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication11/MyWebSite.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data; 
+
+namespace WebApplication11
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBoxCalendar.Text = DateTime.Now.ToString("D", new System.Globalization.CultureInfo("en-US"));
+                TabContainer1.ActiveTabIndex = 0;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!TextBoxEditor.Text.Equals(""))
+            {
+                TabContainer1.ActiveTabIndex = 1;
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TabContainer1.ActiveTabIndex = 2;
+            int numOfFile = calculateNumberOfRecords() + 1;
+            writeEditorTextToFile(numOfFile);
+            addRecordToDatabase(numOfFile);
+            fillAccordion();
+        }
+
+        private void fillAccordion()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select Date, Filename from tblFiles", con);
+                con.Open();
+                using (SqlDataReader saReader = cmd.ExecuteReader())
+                {
+                    while (saReader.Read())
+                    {
+                        string date = saReader.GetString(0);
+                        string filename = saReader.GetString(1);
+                        string fileContent = getFileContent(filename);
+
+                        AjaxControlToolkit.AccordionPane ap1 = new AjaxControlToolkit.AccordionPane();
+                        ap1.ID = "AccordionPane" + Guid.NewGuid().ToString();
+                        ap1.HeaderContainer.Controls.Add(new LiteralControl(date));
+                        ap1.ContentContainer.Controls.Add(new LiteralControl(fileContent));
+                        Accordion1.Panes.Add(ap1);
+                        
+                    }
+                }
+                con.Close();
+            }
+        }
+
+        private string getFileContent(string filename)
+        {
+            String rootPath = Server.MapPath("~");
+            string fileContent = File.ReadAllText(rootPath + "/Data/" + filename);
+            return fileContent;
+        }
+
+        private void addRecordToDatabase(int count)
+        {
+            string cmdString = "INSERT INTO tblFiles (Date, Filename) VALUES (@val1, @val2)";
+            string cs = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                try
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = cmdString;
+                        cmd.Parameters.AddWithValue("@val1", TextBoxCalendar.Text);
+                        cmd.Parameters.AddWithValue("@val2", "file" + count + ".txt");
+                        
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                      
+                catch(SqlException e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+      
+
+        private void writeEditorTextToFile(int count)
+        {
+            string rootPath = Server.MapPath("~");
+            string path = rootPath + "/Data/";
+            using (StreamWriter writer = new StreamWriter(path + "file" + count + ".txt", true))
+            {
+                writer.Write(TextBoxEditor.Text);
+            }
+           
+        }
+
+        private int calculateNumberOfRecords()
+        {
+            int count = 0;
+            string cs = ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select count (*) from tblFiles", con);
+                con.Open();
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            return count;
+        }
+
+        protected void btLoadFile_Click(object sender, EventArgs e)
+        {
+            String rootPath = Server.MapPath("~");
+            string file = File.ReadAllText(rootPath + "/Data/sourceFile.txt");
+            TextBoxEditor.Text = file;
+        }   
+      
+    }
+}
